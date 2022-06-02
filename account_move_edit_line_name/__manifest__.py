3d85ccf924bbc55f3341853e3427e39dcc5adfa0
eamederos <eamederos@gmail.com>
2022-06-02 23:18:07 +0200

    module to edit posted invoices

---
+++ account_move_edit_line_name/__manifest__.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#
+##############################################################################
+
+{
+    'name': 'Account Move Line Edit Name',
+    'version': '14.0.1.0.0',
+    'author': 'Ernesto Mederos',
+    'category': 'Base',
+    'summary': 'Account Move Line Edit Name',
+    'depends': ['account'],
+    'data': [
+    ],
+    'installable': True,
+    'application': True,
+    'demo': [],
+    'test': []
+}
