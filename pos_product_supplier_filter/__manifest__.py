1f5efcff018823e1f2aecdea35bb486b3f9125c0
eamederos <eamederos@gmail.com>
2022-06-13 10:28:33 +0200

    filtrando por proveedores

--- pos_product_supplier_filter/__manifest__.py
+++ pos_product_supplier_filter/__manifest__.py
@@ -3,7 +3,7 @@
 ##############################################################################
 
 {
-    'name': 'POS Product Supplier Filter MFH',
+    'name': 'POS Product Supplier Filter',
     'version': '14.0.1.0.0',
     'author': 'Ernesto Mederos',
     'maintainer': 'Ernesto Mederos',
